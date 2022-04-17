200a516a2702e0026753f6f6bcf28e4d1391c485
Matias Fazio <matias.e.fazio@gmail.com>
2022-04-16 21:10:15 -0300

    commit 1

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//Esto es un comentario
+/* Comentario en varias lineas */
+
+//ES6
+/* let numeroUno;
+
+numeroUno = 1;
+numeroUno = "56";
+let nombre = "Maxi";
+
+const PI = 3.141516;
+
+let nombreUsuario = prompt("ingresa tu nombre");
+ */
+/* alert("hola " + nombreUsuario); */
+
+/* console.log("Hola " + nombreUsuario); */
+console.log(23 + 54);
+
+/*datos ingresados en vsc*/
+
+let persona    = "BART SIMPSON";
+let domicilio  = "742 EVERGREEN";
+let pais       = "USA";
+let nacimiento = "02-11-70";
+const codigo   = "B47U89RE243";
+
+
+let carnet ="codigo: " + codigo + " "+
+            "Nombre: " + persona + " " +
+            "Pais: " + pais + " " +
+            "Nacimiento: " + nacimiento
+            + " " +
+            "Domicilio: " + domicilio;
+
+console.log(carnet);
+
+/*datos ingresados por el usuario */
+
+/* let persona    = prompt("ingresa tu nombre");
+let domicilio  = prompt("ingresa tu domicilio");
+let pais       = prompt("ingresa tu pais");
+let nacimiento = prompt("ingresa tu fecha de nacimiento");
+const codigo   = prompt("ingresa tu dni");
+
+
+let carnet ="codigo: " + codigo + " "+
+            "Nombre: " + persona + " " +
+            "Pais: " + pais + " " +
+            "Nacimiento: " + nacimiento
+            + " " +
+            "Domicilio: " + domicilio; 
+
+console.log(carnet); */
